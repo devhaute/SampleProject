7df31e33505f41ab3f731f318f6950f15a92f398
devhaute <devhaute@gmail.com>
2024-05-19 19:18:58 +0900

    초기 환경 세팅

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by kai on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
